4df641e45954b50e6613866ef6d8ec8b740a4dad
Bl-yadav97 <107970081+Bl1997@users.noreply.github.com>
2022-11-09 22:54:17 +0530

    added_day-2

---
+++ world_helpers/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Home from './Pages.jsx/Home';
+
+function App() {
+  return (
+    <div className="App">
+   
+<Home/>
+  
+    </div>
+  );
+}
+
+export default App;
